98304a233ce9039ac7d5449640168f0e6b27fd4f
Олег <@DESKTOP-T6VKS18>
2022-07-20 12:58:26 +0300

    Добавьте файлы проекта.

---
+++ Падежи/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    enum eVariation { рубль, рубля, рублей };
+
+    static string CurrencyVariation(int iNumber)
+    {
+        eVariation evResult;
+
+        if ((iNumber / 10) % 10 == 1)   
+            evResult = eVariation.рублей;
+        else
+            switch (iNumber % 10)       
+            {
+                case 1:
+                    evResult = eVariation.рубль;
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    evResult = eVariation.рубля;
+                    break;
+                default:
+                    evResult = eVariation.рублей;
+                    break;
+            }
+
+        return evResult.ToString();
+    }
+
+    static void Main()
+    {
+        for (int i = 0; i < 24; i++)
+            Console.WriteLine("{0} {1}", i, CurrencyVariation(i));
+        Console.ReadLine();
+    }
+}
